8937fd44291dae0e7215ea368025500366ad92c1
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-04-23 21:14:22 +0300

    global methods

--- src/potholeClassifier/__init__.py
+++ src/potholeClassifier/__init__.py
@@ -5,7 +5,7 @@ import logging
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 
 log_dir = "logs" 
-log_file_path = os.path.join(log_dir, "logs.log") 
+log_file_path = os.path.join(log_dir, "logfile.log") 
 os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
