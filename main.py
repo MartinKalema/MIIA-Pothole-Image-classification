e1db56311b3fe5ca449f06460fa2e2a550e7389c
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-04-23 21:58:39 +0300

    Modularized Data Ingestion

---
+++ main.py
@@ -0,0 +1,16 @@
+from potholeClassifier import logger
+from potholeClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info("*********************************\n")
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(
+        f">>>>>> {STAGE_NAME} completed <<<<<<<\n**********************************")
+except Exception as e:
+    logger.exception(e)
+    raise e
