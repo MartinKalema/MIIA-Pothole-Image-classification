c177676ab124eb6a4ed25d65f0e6c210e47a46c9
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-04-29 13:25:53 +0300

    Added training component to pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from potholeClassifier import logger
 from potholeClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from potholeClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from potholeClassifier.pipeline.stage_03_model_training import TrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -21,6 +22,19 @@ try:
     logger.info("*********************************\n")
     logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(
+        f">>>>>> {STAGE_NAME} completed <<<<<<<\n **********************************")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Training Stage"
+
+try:
+    logger.info("*********************************\n")
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    obj = TrainingPipeline()
     obj.main()
     logger.info(
         f">>>>>> {STAGE_NAME} completed <<<<<<<\n **********************************")
