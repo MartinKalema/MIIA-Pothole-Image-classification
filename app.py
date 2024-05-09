c74b504d44d653500df3f726b1e3267c53d51782
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-05-10 01:53:24 +0300

    Document methods

--- app.py
+++ app.py
@@ -9,21 +9,37 @@ from pathlib import Path
 
 
 class Classifier:
-    def __init__(self, config_file_path):
+    def __init__(self, config_file_path: Path):
+        """
+        Constructor method for the Classifier class.
+
+        Args:
+            config_file_path (Path): Path to the configuration file.
+        """
         self.config_file_path = config_file_path
         self.config = read_yaml(self.config_file_path)
         self.class_names = ["No Pothole", "Has Pothole"]
         self.model = None
 
-    def load_best_model(self):
+    def load_best_model(self) -> None:
+        """
+        Method to load the best trained model from the specified path.
+        """
         if self.model is None:
             self.model = load_model(self.config.training.trained_model_path)
-        return self.model
 
-    def import_and_predict(self, image_data):
+    def import_and_predict(self, image_data: Image) -> np.ndarray:
+        """
+        Method to preprocess the image data and make predictions using the loaded model.
+
+        Args:
+            image_data (Image): Input image data.
+
+        Returns:
+            np.array: Predicted class probabilities.
+        """
         if self.model is None:
             raise ValueError("Model not loaded. Please load the model first.")
-
         size = (224, 224)
         image = ImageOps.fit(image_data, size)
         image = np.asarray(image)
@@ -32,7 +48,16 @@ class Classifier:
         prediction = self.model.predict(img_reshape)
         return prediction
 
-    def classify_image(self, image):
+    def classify_image(self, image: Image) -> str:
+        """
+        Method to classify the input image.
+
+        Args:
+            image (Image): Input image data.
+
+        Returns:
+            str: Classification result text.
+        """
         predictions = self.import_and_predict(image)
         score = tf.nn.softmax(predictions[0])
         result_text = "This image most likely belongs to the <b>{}</b> class.".format(
@@ -41,20 +66,36 @@ class Classifier:
 
 
 class StreamlitApp:
-    def __init__(self, classifier):
+    def __init__(self, classifier: Classifier):
+        """
+        Constructor method for the StreamlitApp class.
+
+        Args:
+            classifier (Classifier): Instance of the Classifier class.
+        """
         self.classifier = classifier
 
-    def run(self):
-        st.markdown("## Pothole Image Classification", unsafe_allow_html=True)
-
+    def load_model(self) -> None:
+        """
+        Method to run the Streamlit application.
+        """
         with st.spinner('Model is being loaded..'):
             self.classifier.load_best_model()
 
+    def display_intro(self) -> None:
+        """Display the intro text"""
+
+        st.markdown("## Pothole Image Classification", unsafe_allow_html=True)
         st.write("""
         Potholes are fatal and can cause severe damage to vehicles as well as can cause deadly accidents. In South Asian countries, pavement
         distresses are the primary cause due to poor subgrade conditions, lack of subsurface drainage, and excessive rainfalls. This prediction service classifies images to find whether they have potholes or not.
         """)
 
+    def run(self) -> None:
+        """Run the app"""
+
+        self.load_model()
+        self.display_intro()
         file = st.file_uploader(
             "Please upload the image file", type=[
                 "jpg", "png"])
--- lint.py
+++ lint.py
@@ -11,6 +11,15 @@ ROOT_DIR = os.getenv("ROOT_DIR")
 
 
 def get_all_python_files(directory: str) -> List[str]:
+    """
+    Get all Python files within a given directory and its subdirectories.
+
+    Args:
+        directory (str): The directory to search for Python files.
+
+    Returns:
+        List[str]: A list of file paths to Python files.
+    """
     python_files = []
     for root, _, files in os.walk(directory):
         for file in files:
@@ -20,6 +29,15 @@ def get_all_python_files(directory: str) -> List[str]:
 
 
 def execute_command(command: List[str]) -> subprocess.CompletedProcess:
+    """
+    Execute a command in the shell.
+
+    Args:
+        command (List[str]): The command and its arguments to execute.
+
+    Returns:
+        subprocess.CompletedProcess: The completed subprocess.
+    """
     try:
         process = subprocess.run(command, capture_output=True, text=True)
         return process
@@ -29,6 +47,12 @@ def execute_command(command: List[str]) -> subprocess.CompletedProcess:
 
 
 def lint_python_file(file: str) -> None:
+    """
+    Lint a Python file using autopep8 and flake8.
+
+    Args:
+        file (str): Path to the Python file to lint.
+    """
     autopep8_process = execute_command(
         ['autopep8', '--in-place', '--aggressive', '--aggressive', file])
     if autopep8_process.returncode != 0:
@@ -46,6 +70,9 @@ def lint_python_file(file: str) -> None:
 
 
 def main():
+    """
+    Main function to lint all Python files in the project directory.
+    """
     project_directory = ROOT_DIR
     all_python_files = get_all_python_files(project_directory)
     for file in all_python_files:
