1204786ed528d04bcb922f8fe525d9290d927154
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-05-10 14:09:36 +0300

    unit tests

--- tests/test_common_methods.py
+++ tests/test_common_methods.py
@@ -4,6 +4,10 @@ from potholeClassifier.constants import *
 from box import ConfigBox
 
 
-def test_read_yaml() -> None:
+def test_read_yaml_valid_file() -> None:
     result = read_yaml(CONFIG_FILE_PATH)
     assert isinstance(result, ConfigBox)
+
+def test_read_yaml_nonexistent_file() -> None:
+    with pytest.raises(FileNotFoundError):
+        read_yaml(Path(EMPTY_FILE_PATH))
